11f7dc02686f546ab8dd1463cbda29cf31e0b6c5
deithh <jasiuc40@gmail.com>
2021-04-25 11:29:11 +0200

    ants movement fixed

--- ants.py
+++ ants.py
@@ -1,63 +1,51 @@
 
-from models import *
+from models import ant
+from models import anthill
+
+from config import *
+
 import pygame
 import keyboard as kb
 import time
 
-
-
-
 #simulation
 RUN = True
 CLOCK = pygame.time.Clock()
-#colors
-BLACK = (0, 0, 0)
-ANT_COLOR = (56, 38, 38)
-FOOD_COLOR = (255,0,0)
+IMG = {}
 
 
 
-def _window():
-    global SIZE, BLACK
-    window = pygame.display.set_mode((SIZE*10, SIZE*10))
+def load_textures():
+    IMG['ant'] = pygame.transform.scale(pygame.image.load('textures/'+'ant'+'.png'), (MUL,MUL))
+
+def window_():
+    window = pygame.display.set_mode((SIZE * MUL, SIZE * MUL))
     pygame.display.set_caption("ants")
     window.fill(BLACK)
     return window
 
-def draw(window, map):
-    global SIZE, ANT_COLOR, BLACK
-    for x in range(SIZE):
-        for y in range(SIZE):
-            color = BLACK
-            if map[x, y, 3]:
-                color = ANT_COLOR
-            elif map[x,y,0]:
-                color = FOOD_COLOR
-            elif map[x,y,1] or map[x,y,2]:
-                color = (0, min(map[x,y,1],255),min(map[x, y,2],255))
+def draw(window, anthill, ants):
+    anthill.draw(window)
+    for ant in ants:
+        ant.draw(window, IMG['ant'])
 
-            if color == ANT_COLOR:
-                pygame.draw.rect(window, color, [x*10+3,y*10+3,4,4],0)
-            else:
-                pygame.draw.rect(window, color, [x*10,y*10,10,10],0)
-
-def new (x):
-    global  SPAWNPOINT
-    x = ant(*SPAWNPOINT)
+def new (x, anthill):
+    x = ant(*SPAWNPOINT, anthill)
     return x
+
 def main():
-    global RUN, ANTS, CLOCK
+    global RUN
+    load_textures()
+    Anthill = anthill(SIZE)
+    # Anthill.set_food(0,0,1,100,100)
+    # Anthill.set_food(0,0,100,1,100)
+    # Anthill.set_food(99,0,1,100,100)
+    # Anthill.set_food(0,99,100,1,100)
+    Anthill.set_food(10,10,10,10,100)
 
-    map = anthill(SIZE)
-    map.set_food(0,0,1,100,10)
-    map.set_food(0,0,100,1,10)
-    map.set_food(99,0,1,100,10)
-    map.set_food(0,99,100,1,10)
-    #map.set_food(0,0,10,10,10)
-
-    ants = [new(i) for i in range(ANTS)]
+    ants = [new(i, Anthill) for i in range(ANTS)]
     pygame.init()
-    window = _window()
+    window = window_()
 
 
     while RUN:
@@ -65,15 +53,15 @@ def main():
         pygame.event.get()
 
         for ant in ants:
-            ant.move(map.map)
-        map.update(ants)
+            ant.move()
+        Anthill.update(ants)
 
         if kb.is_pressed('esc'):
             pygame.quit()
             RUN = False
-        draw(window, map.map)
+        draw(window, Anthill, ants)
         pygame.display.flip()
-        CLOCK.tick(300)
+        CLOCK.tick(FPS)
 
 
 
--- models.py
+++ models.py
@@ -2,76 +2,65 @@
 import numpy as np
 import math
 import random
+import pygame
+
 from scipy.ndimage.filters import gaussian_filter
-
-################## config #####################
-#map
-SIZE = 100
-ANTS = 500
-MAX_P = 200
-P_EVAPOR = .0146
-P_TRAVEL = .39
-#ants
-SPAWNPOINT = (SIZE//2, SIZE//2)
-P_STRENGTH = 12
-P_WEAK = .0025
-
-################## config #####################
-
-FOOD = 0
-
-DIRECTIONS = {0: (0,1), 1: (1,1), 2: (1,0), 3: (1,-1), 4: (0,-1), 5: (-1,-1), 6: (-1,0), 7: (-1,1)}
-C_DIRECTIONS ={(0,1): 0, (1,1): 1, (1,0): 2, (1,-1): 3, (0,-1): 4, (-1,-1): 5, (-1,0): 6, (-1,1): 7}
+from config import *
+def rotate(image, direction):
+    angle = -direction * 45 - 90
+    new = pygame.transform.rotate(image, angle)
+    return new
 
 class ant:
-    def __init__(self, x, y):
-        global RADIUS, P_STRENGTH
-        self.__home = (x, y)
+    def __init__(self, x, y, anthill):
         self.__x = x
         self.__y = y
         self.__backpack = False
         self.__fs = P_STRENGTH
         self.__direction = random.randint(0,7) #pointer to ['N','NE','E','SE', 'S', 'SW', 'W', 'NW']
+        self.__anthill = anthill
 
-    def leave_trace(self, map):
-        global MAX_P, P_WEAK
-        if self.__backpack:
-            if map[self.__x, self.__y, 2] > MAX_P:
-                pass
-            else:
-                map[self.__x, self.__y, 2] += self.__fs
-        else:
-            if map[self.__x, self.__y, 1] > MAX_P:
-                pass
-            else:
-                map[self.__x, self.__y, 1] += self.__fs
+    def weaken_signal(self):
         self.__fs *= (1-P_WEAK)
 
-    def check_colisions(self, x, y, map):
-        global SIZE
+    def leave_trace(self):
+        if self.__backpack:
+            if self.__anthill.map[self.__x, self.__y, 2] + self.__fs < MAX_P:
+                self.__anthill.map[self.__x, self.__y, 2] += self.__fs
+        else:
+            try:
+                if self.__anthill.map[self.__x, self.__y, 1] + self.__fs < MAX_P:
+                    self.__anthill.map[self.__x, self.__y, 1] += self.__fs
+            except :
+                print (self.__x)
+                print (self.__y)
+                raise
+        self.weaken_signal()
 
-        if self.__x + x < 0 or self.__x + x > SIZE - 1:
+    def check_colisions(self, x, y):
+        #walls colisions
+        if self.__x + x < 0 or self.__x + x > self.__anthill.size - 2:
             return False
-        if self.__y + y < 0 or self.__y + y > SIZE - 1:
+        if self.__y + y < 0 or self.__y + y > self.__anthill.size - 2:
             return False
         return True
 
-    def find_target(self, map):
-        global SIZE, DIRECTIONS, C_DIRECTIONS
-        right = self.__direction + 1
-        if right > 7:
-            right = 0
-        left = self.__direction - 1
-        if left < 0:
-            left = 7
+    def find_target(self):
 
-        moves = [DIRECTIONS[self.__direction], DIRECTIONS[left], DIRECTIONS[right]] #<<<<<<<<<
+        right = self.cyclometric_direction(self.__direction, 1)
+
+        left = self.cyclometric_direction(self.__direction, -1)
+
+
+        moves = [DIRECTIONS[self.__direction], DIRECTIONS[left], DIRECTIONS[right]]
+        random.shuffle(moves)
         valid = []
+
         for i, move in enumerate(moves):
             x, y = move
-            if (self.__x +x, self.__y + y) == self.__home:
-                return self.__x +x, self.__y + y
-            if self.check_colisions(x,  y, map):
+            if self.__anthill.in_nest(self.__x +x, self.__y + y):
+                return C_DIRECTIONS[(x, y)]
+            if self.check_colisions(x,  y):
                 valid.append((x + self.__x, y + self.__y))
 
 
@@ -79,108 +68,147 @@ class ant:
         if not self.__backpack:
 
             for x, y in valid:
-                if map[x, y, 0]:
+                if self.__anthill.map[x, y, 0]:
 
-                    return x, y
+                    return C_DIRECTIONS[(x-self.__x, y-self.__y)]
 
-                if map[x, y, 2] > max_[2] and map[x, y, 2] > .1:
-                    max_ = [x, y, map[x, y, 2]]
+                if self.__anthill.map[x, y, 2] > max_[2] and self.__anthill.map[x, y, 2] > ANT_SENSE:
+                    max_ = [x, y, self.__anthill.map[x, y, 2]]
         else:
             for x, y in valid:
-                if map[x, y, 1] > max_[2]:
-                    max_ = [x, y, map[x, y, 1]]
+                if self.__anthill.map[x, y, 1] > max_[2]:
+                    max_ = [x, y, self.__anthill.map[x, y, 1]]
         if max_[2]:
 
-            return max_[0], max_[1]
+            return C_DIRECTIONS[(max_[0]-self.__x, max_[1]-self.__y)]
 
+    def grab_food(self, ):
 
-
-    def grab_food(self, map):
-        global P_STRENGTH
         if not self.__backpack:
-            if map[self.__x, self.__y, 0]:
+            if self.__anthill.map[self.__x, self.__y, 0]:
                 self.__backpack = True
-                map[self.__x, self.__y, 0] -=1
+                self.__anthill.map[self.__x, self.__y, 0] -=1
                 self.change_direction(-4)
                 self.__fs = P_STRENGTH
 
-
     def leave_food(self):
-        global FOOD, P_STRENGTH
-        if (self.__x, self.__y) == self.__home and self.__backpack:
+        if self.__anthill.in_nest(self.__x, self.__y) and self.__backpack:
+
                 self.__backpack = False
                 self.change_direction(-4)
                 self.__fs = P_STRENGTH
-                FOOD +=1
-                print (FOOD)
+                self.__anthill.food_update(1)
+
     def change_direction(self, value = 0):
 
         temp = random.choice([-1,0,1]) #direction change left/straigth/right
         if value:
             temp = value
+        self.__direction = self.cyclometric_direction(self.__direction, temp)
 
-        if self.__direction + temp > 7:
-            self.__direction = 0 + temp - 1
-        elif self.__direction + temp < 0:
-            self.__direction = 7 + temp + 1
-        else:
-            self.__direction += temp
+    def move_forward(self):
 
-
-
-    def move_forward(self, map):
-        global  DIRECTIONS
         x, y = DIRECTIONS[self.__direction]
-        while not self.check_colisions(x, y, map):
-            self.change_direction(random.choice([3,4,-3]))
+        if not self.check_colisions(x, y):
+            self.change_direction(4)
+            x, y = DIRECTIONS[self.__direction]
+        if not self.check_colisions(x, y):
+            self.change_direction(random.choice([1, -1]))
             x, y = DIRECTIONS[self.__direction]
         self.__x += x
         self.__y += y
 
-    def move(self, map):
-        if self.find_target(map) and random.randint(0,10):
-
-            x, y = self.find_target(map)
-            self.__x = x
-            self.__y = y
+    def move(self):
+        if self.find_target():
+            self.__direction = self.find_target()
+            self.move_forward()
 
 
         else:
-            self.move_forward(map)
+            self.move_forward()
             self.change_direction()
 
-        self.leave_trace(map)
-        self.grab_food(map)
+        self.leave_trace()
+        self.grab_food()
         self.leave_food()
+
+    def draw(self, window, texture):
+        sprite = rotate(texture, self.__direction)
+        window.blit(sprite, pygame.Rect(self.__y * MUL + random.randint(-MUL//2, MUL//2), self.__x * MUL+ random.randint(-MUL//2, MUL//2), MUL, MUL))
+
+    @property
+    def direction(self):
+        return self.__direction
 
     @property
     def cords(self):
         return (self.__x, self.__y)
 
+    @staticmethod
+    def cyclometric_direction(direction, rotation):
+
+        return (direction + rotation) % 8
+
 
 class anthill:
-    def __init__(self, size):
+    def __init__(self, size,):
         #food, backtrack, foodpath, ants
-        self.map = np.full((size, size, 4), 0.0)
-        self.size = size
-
+        self.__map = np.full((size+1, size+1, 3), 0.0)
+        self.__size = size
+        self.__frame = 0
+        self.__nest = (size//2, size//2)
+        self.__nest_radius = 5
+        self.__food = 0
 
     def update(self, ants):
-        global P_EVAPOR, P_TRAVEL
 
         #trace evaporating
-        self.map[:, :, 1:3] *= (1-P_EVAPOR)
-        self.map[:,:, 1] = gaussian_filter(self.map[:,:, 1], P_TRAVEL)
-        self.map[:,:, 2] = gaussian_filter(self.map[:,:, 2], P_TRAVEL)
-        #remove ants
-        self.map[:, :, 3] = 0
+        self.__map[:, :, 1:3] *= (1-P_EVAPOR)
+        if self.__frame % 5 == 0:
+            self.__map[:,:, 1] = gaussian_filter(self.__map[:,:, 1], P_TRAVEL)
+            self.__map[:,:, 2] = gaussian_filter(self.__map[:,:, 2], P_TRAVEL)
 
-        #place ants on new cords
-        for i in ants:
-            x, y = i.cords
-            self.map[x, y, 3] += 1
 
+
+        self.__frame+=1
+        print(self.__food)
 
     def set_food(self, x, y, width, height, quantity):
 
-        self.map[x:x+width, y:y+height, 0] = quantity
+        self.__map[x:x+width, y:y+height, 0] = quantity
+
+    def draw_nest(self, window):
+        x = self.__nest[0] * MUL
+        y = self.__nest[1] * MUL
+        pygame.draw.circle(window, NEST_COLOR, (x, y), self.__nest_radius * MUL)
+
+    def draw(self, window):
+        for x in range(SIZE):
+            for y in range(SIZE):
+                color = BLACK
+                if self.__map[x,y,0]:
+                    color = FOOD_COLOR
+                elif self.__map[x,y,1] or self.__map[x,y,2]:
+                    color = (0, min(self.__map[x,y,1],255),min(self.__map[x, y,2],255))
+                pygame.draw.rect(window, color, [y * MUL,x * MUL, MUL, MUL],0)
+        self.draw_nest(window)
+
+    @property
+    def map(self):
+        return self.__map
+
+    @property
+    def size(self):
+        return self.__size
+
+    def food_update(self, value = 1):
+        self.__food +=1
+
+    def in_nest(self, ax, ay): #ant x, y cords
+        nx, ny = self.__nest #nest x, y cords
+        if math.sqrt((nx - ax)**2 + (ny - ay)**2) < self.__nest_radius:
+            return True
+
+        return False
+
+
